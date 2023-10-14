dd9205e1d2539f58cc84501a6359f2e6630ef0c3
Ricardo Simines Scopim <132607428+rscopim@users.noreply.github.com>
2023-10-14 12:23:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const body = document.querySelector("body");
+
+body.innerHTML = `
+    <h1 id="titulo">MÁQUINA DE CAFÉ EXPRESSO</h1>
+    <img src="https://dular.vteximg.com.br/arquivos/ids/5572903-1000-1000/image-6311164f9a9f4f9482836b8fa9eb302c.jpg?v=638095640654700000" alt="imagem">
+    <h2>MÁQUINA DE CAFÉ EXPRESSO 15 BAR TANQUE DE 1,5 LITROS COM VAPORIZADOR DE LEITE 1300W, 110V, SHARDOR CJ 265EB, PRETO.</h2>
+    <h3>Uma ou duas doses de café expresso Inclui um filtro de aço inoxidável de dose única e dupla para escolher o tamanho da dose de café expresso. </h3>
+    <ul>
+        <li>1 Shot Brew cerca de 40ml, 1.35oz de café. </li>
+        <li>2 Shot Brew cerca de 80ml, 2.7oz de café.</li>
+        <li>Pressione e segure o botão por 3s para fazer uma xícara dupla de café expresso.</li>
+    </ul>
+    <p>O preço já inclui todos os custos, não será cobrado nenhum valor extra para o recebimento do produto</p>
+`
